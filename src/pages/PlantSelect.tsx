da01fcc6b30b0ed9c10d594a88e7ad3b38bc4046
gnrfilipe04 <gnr_filipe@hotmail.com>
2021-04-23 23:12:52 -0300

    :sparkles: Passing params per routes

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -10,13 +10,14 @@ import { PlantCardPrimary } from '../components/PlantCardPrimary'
 import colors from '../styles/colors'
 import fonts from '../styles/fonts'
 import { Load } from '../components/Load'
+import { useNavigation } from '@react-navigation/core'
 
 interface EnviromentProps {
     key: string;
     title: string;
 }
 
-interface PlantsProps {
+interface PlantProps {
     id: string,
     name: string,
     about: string,
@@ -30,15 +31,15 @@ interface PlantsProps {
 }
 
 export function PlantSelect(){
+    const navigation = useNavigation()
     const [enviroments, setEnviroments] = useState<EnviromentProps[]>([])
-    const [plants, setPlants] = useState<PlantsProps[]>([])
-    const [filteredPlants, setFilteredPlants] = useState<PlantsProps[]>([])
+    const [plants, setPlants] = useState<PlantProps[]>([])
+    const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>([])
     const [enviromentSelected, setEnviromentSelected] = useState('all')
     const [loading, setLoading] = useState(true)
 
     const [page, setPage] = useState(1)
     const [loadingMore, setLoadingMore] = useState(false)
-    const [loadedAll, setLoadedAll] = useState(false)
 
     async function fetchPlants(){
         const { data } = await api
@@ -79,6 +80,10 @@ export function PlantSelect(){
         setPage(oldValue => oldValue + 1)
         fetchPlants()
 
+    }
+
+    function handlePlantSelect(plant: PlantProps){
+        navigation.navigate('PlantSave', { plant })
     }
 
     useEffect(() => {
@@ -125,6 +130,7 @@ export function PlantSelect(){
 
             <FlatList 
                 data={enviroments}
+                keyExtractor={(item) => String(item.key)}
                 renderItem={({item }) => (
 
                     <EnviromentButton 
@@ -143,9 +149,13 @@ export function PlantSelect(){
             <View style={styles.plants}>
                 <FlatList
                 data={filteredPlants}
+                keyExtractor={(item) => String(item.id)}
                 renderItem={({item }) => (
 
-                    <PlantCardPrimary data={item}/>
+                    <PlantCardPrimary 
+                        data={item}
+                        onPress={() => handlePlantSelect(item)}
+                    />
                 )}
                 showsVerticalScrollIndicator={false}
                 numColumns={2}
--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -6,6 +6,7 @@ import { Welcome } from '../pages/Welcome'
 import { UserIdentification } from '../pages/UserIdentification'
 import { Confirmation } from '../pages/Confirmation'
 import { PlantSelect } from '../pages/PlantSelect'
+import { PlantSave } from '../pages/PlantSave'
 
 import colors from '../styles/colors'
 
@@ -38,7 +39,12 @@ const AppRoutes: React.FC = () => (
         <stackRoutes.Screen 
             name="PlantSelect"
             component={PlantSelect}
-        />  
+        />
+
+        <stackRoutes.Screen 
+            name="PlantSave"
+            component={PlantSave}
+        /> 
 
     </stackRoutes.Navigator>
 )
